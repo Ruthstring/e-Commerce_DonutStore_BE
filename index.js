07209e434c362b88cbc15b423153dd6e612e1064
Ruth Cuellar <ruthcllp@gmail.com>
2024-10-04 19:41:00 +0200

    cors modified for deployment

--- index.js
+++ index.js
@@ -13,10 +13,18 @@ const featuredRoutes=require("./routes/featuredRoutes.js")
 const app=express();
 const port=process.env.PORT || 5000;
 
+const allowedOrigins = ['http://localhost:5173', 'https://donutstore-e-commerce.netlify.app'];
+
 //MIDDLEWARE
 // app.use(cors());
 app.use(cors({
-    origin: 'http://localhost:5173',  // Allows frontend origin
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      }, 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  
     allowedHeaders: ['Authorization', 'Content-Type'],  
     credentials: true 
